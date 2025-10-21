1670a47d1d201043d6de9309734fe6f5f7ce7786
bluven <yanshiyi1983@163.com>
2025-10-21 15:35:19 +0800

    Add telemetry

--- app/instance/taskq/internal/config/config.go
+++ app/instance/taskq/internal/config/config.go
@@ -1,15 +1,15 @@
 package config
 
 import (
+	"github.com/zeromicro/go-zero/core/service"
 	"github.com/zeromicro/go-zero/core/stores/redis"
-	"github.com/zeromicro/go-zero/rest"
 	"github.com/zeromicro/go-zero/zrpc"
 
 	"github.com/bluven/f-cloud/pkg/gormx"
 )
 
 type Config struct {
-	rest.RestConf
+	service.ServiceConf
 
 	Redis redis.RedisConf
 	MySQL gormx.MySQLConfig
